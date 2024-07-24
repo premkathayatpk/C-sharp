8b0fbbea843158679dd346a4bf950a51753af7e5
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-07-24 22:17:59 +0545

    properties

---
+++ Lab3/Properties.cs
@@ -0,0 +1,24 @@
+using System;
+class Student{
+  public string Name{
+    get;
+    set;
+  }
+   public int Age{
+    get;
+    set;
+   }
+   public string Faculty{
+    get;
+    set;
+   }
+}
+class Properties{
+  static void Main(){
+    Student s=new Student();
+    s.Name="Ram";
+    s.Age=22;
+    s.Faculty=20000;
+    Console.WriteLine(s.Name+" "+s.Age+" "+s.Faculty);
+  }
+}
